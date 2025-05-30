75c942ecb7889d7cf61dc0556acb26a2756a454f
Bruce Chen <aaaeeettt124@126.com>
2025-05-30 19:41:50 +0800

    ls

--- typescript/basic.ts
+++ typescript/basic.ts
@@ -16,9 +16,9 @@ const square = (x: number): number => x * x;
 const double = (x: number): number => x * 2;
 
 const compose =
-  <T, U, V>(f: (x: U) => V, g: (x: T) => U) =>
-  (x: T): V =>
-    f(g(x));
+  <T, U, V>(第一函数: (x: U) => V, 第二函数: (x: T) => U) =>
+  (输入值: T): V =>
+    第一函数(第二函数(输入值));
 
 const result = compose(square, double)(5); // (5*2)^2 = 100
 console.log("Composed Result:", result);
