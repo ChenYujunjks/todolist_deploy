f8e3660f47773105cffd6c890c7708796e78ed25
Bruce Chen <aaaeeettt124@126.com>
2025-06-17 17:38:33 +0800

    adding tracking ip address of server v1

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { todoRouter } from "./todos";
 import { formRouter } from "./form";
+import { coinbaseRouter } from "./coinbase";
 
 export const appRouter = router({
   todo: todoRouter,   // 前端调用时路径为 todo.getTodos 等
   form: formRouter,   // form.formSubmit
+  coinbase: coinbaseRouter,
 });
 
 // 导出类型供前端自动推导
