ccdbf03cdb1159fe3dfbfab434e7b08823cfe31f
Bruce Chen <aaaeeettt124@126.com>
2025-07-05 14:19:16 +0800

    deleting vercel json since app router

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,3 +1,8 @@
+// ✅ 自动部署到最近边缘节点（Edge Network）
+export const runtime = "edge";
+
+console.log("Running in:", process.env.VERCEL_REGION || "unknown");
+
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 import { appRouter } from "@/server";
