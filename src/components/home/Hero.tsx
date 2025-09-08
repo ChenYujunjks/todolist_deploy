6965fecbc4b5db03506843e40cdd203547b52204
Bruce Chen <aaaeeettt124@126.com>
2025-09-08 17:45:53 -0400

    fixed a mistake for vercel eslint

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
       >
-        👋 Hi, I'm <span className="text-[--color-brand]">Bruce Chen</span>
+        {`👋 Hi, I'm`} <span className="text-[--color-brand]">Bruce Chen</span>
       </motion.h1>
 
       <motion.p
